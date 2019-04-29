f55c2739ffda223352cbca5b9ff62b0a89dcd289
Ayoung Kim <ayoung.t.kim@gmail.com>
2019-04-29 13:56:11 +0900

    ak: cleaning matlab

--- matlab/exposure_control/compute_next_exp_gpgo.m
+++
@@ -1,9 +0,0 @@
-function next_t = compute_next_exp_binary (curr_t)
-%
-%-----------------------------------------------------------------
-%    History:
-%    Date            Who         What
-%    -----------     -------     -----------------------------
-%    2017.08.28      ak          created and written
-
-next_t = curr_t;
--- matlab/exposure_gain_control/gp/functions/gp_cov_k_POLY.m
+++
@@ -1,9 +0,0 @@
-function Kij = gp_cov_k_POLY (Xi, Xj, p, s0, cfg)
-%
-%-----------------------------------------------------------------
-%    History:
-%    Date            Who         What
-%    -----------     -------     -----------------------------
-%    2013.12.26      ak          created and written
-
-Kij = (Xi'*Xj + s0*s0).^p;
